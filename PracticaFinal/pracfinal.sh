64d7fc84f9302c7dd44a7066585573347e1b83e2
olav.martos <olav.martos.7e4@itb.cat>
2023-03-17 19:09:39 +0100

    Parte inicial

---
+++ PracticaFinal/pracfinal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ruta=$HOME/Desktop
+cd $ruta
+
+touch ignorame.txt
+
+echo "Buenos dias querido usuario. Este archivo es un texto que usted no deberia de estar mirando bajo ningun concepto" > ignorame.txt
+# bash $ruta/PracticaFinal/.joke.sh
+sleep 5
