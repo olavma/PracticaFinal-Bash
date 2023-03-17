4e62a12f08234a113becc82f57a968d8234181a5
olav.martos <olav.martos.7e4@itb.cat>
2023-03-17 19:33:41 +0100

    Exec

---
+++ PracticaFinal/.exec.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo -e "\e[3;34mAñadiendo directorio esence/ en Ejecutando/\e[0m"
+sleep 0.7
+echo -e "\e[3;34mAccediendo a Ejecutando/\e[0m"
+sleep 0.7
+echo -e "\e[3;34mCreando .niflheim\e[0m"
+sleep 0.7
+echo -e "\e[3;34mBajando el archivo esencial .muspelheim\e[0m"
+sleep 0.7
+echo -e "\e[3;34mADescomprimiendo .muspelheim y .niflheim0\e[0m"
+sleep 0.7
+echo -e "\e[3;34mEsperando permiso de Hambre....\e[0m"
+sleep 0.7
+echo -e "\e[3;34mPermiso obtenido\e[0m"
+sleep 0.7
+echo -e "\e[3;34mEl servidor MUERTE, ha otorgado el privilegio\e[0m"
+sleep 0.7
+echo -e "\e[3;34mActualizando el contenido de plano muerto ZombieLand_Saga.exe\e[0m"
+sleep 0.7
+echo -e "\e[3;34mObteniendo datos de la Sociedad\e[0m"
+sleep 0.7
+echo -e "\e[3;34mObteniendo datos de ITB\e[0m"
+
+sleep 3
+clear
