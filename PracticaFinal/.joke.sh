3b81295eb171ac192279fa0a82f44623db04203a
olav.martos <olav.martos.7e4@itb.cat>
2023-03-17 19:22:06 +0100

    Joke

---
+++ PracticaFinal/.joke.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e "\e[5;36mignorame.txt ............... Proceso 1/100 completados\e[0m"
+sleep 3
+echo -e "\e[5;36mignorame.txt ............... Proceso 20/100 completados\e[0m"
+sleep 3
+echo -e "\e[5;36mignorame.txt ............... Proceso 60/100 completados\e[0m"
+sleep 3
+echo -e "\e[5;36mignorame.txt ............... Proceso 100/100 completados\e[0m"
+sleep 6
--- PracticaFinal/pracfinal.sh
+++ PracticaFinal/pracfinal.sh
@@ -6,5 +6,5 @@ cd $ruta
 touch ignorame.txt
 
 echo "Buenos dias querido usuario. Este archivo es un texto que usted no deberia de estar mirando bajo ningun concepto" > ignorame.txt
-# bash $ruta/PracticaFinal/.joke.sh
+bash $ruta/PracticaFinal/.joke.sh
 sleep 5
