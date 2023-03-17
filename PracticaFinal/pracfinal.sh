71736b8677f28a952842266a1767753ca5491ba6
olav.martos <olav.martos.7e4@itb.cat>
2023-03-17 19:29:32 +0100

    Antes del exec

--- PracticaFinal/pracfinal.sh
+++ PracticaFinal/pracfinal.sh
@@ -7,4 +7,13 @@ touch ignorame.txt
 
 echo "Buenos dias querido usuario. Este archivo es un texto que usted no deberia de estar mirando bajo ningun concepto" > ignorame.txt
 bash $ruta/PracticaFinal/.joke.sh
+echo "Lo digo de verdad. ¿Que haces cotilleando?" >> ignorame.txt
 sleep 5
+echo "¿De verdad te esperas que haya algo importante en este archivo?" >> ignorame.txt
+sleep 5
+echo "Pues NO. Estas perdiendo el tiempo, chico/chica/¿chique?" >> ignorame.txt
+
+touch .archivo.sh
+echo "#!/bin/bash" > .archivo.sh
+mkdir Ejecutando
+bash $ruta/PracticaFinal/.exec.sh
